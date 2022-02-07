5e2bf308a147b0c7d3e656ff91c7d16048689bdf
Louis-Justin TALLOT <lj.tallot@gmail.com>
2022-02-07 20:48:28 +0100

    Useful compiling scripts

---
+++ Presentation/compile.sh
@@ -0,0 +1,4 @@
+xelatex presentation -halt-on-error 
+biber presentation  
+xelatex presentation -halt-on-error 
+xelatex presentation -halt-on-error 
---
+++ Rapport/compile.sh
@@ -0,0 +1,4 @@
+xelatex report_therapy_monoclonal_antibodies -halt-on-error 
+biber report_therapy_monoclonal_antibodies
+xelatex report_therapy_monoclonal_antibodies -halt-on-error 
+xelatex report_therapy_monoclonal_antibodies -halt-on-error 
